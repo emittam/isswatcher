93c72cf654035e71da007c04f0b65ec3078c18a2
Nersch <stellasurround@outlook.com>
2015-04-12 01:45:56 +0900

    [UI]註侮ｏ翌ｂｂ

--- app/src/main/java/net/emittam/isswatcher/MainActivity.java
+++ app/src/main/java/net/emittam/isswatcher/MainActivity.java
@@ -1,19 +1,68 @@
 package net.emittam.isswatcher;
 
-import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    // GoogleMapオブジェクトの宣言
+    private GoogleMap googleMap;
+
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // MapFragmentオブジェクトを取得
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+
+        try {
+            // GoogleMapオブジェクトの取得
+            googleMap = mapFragment.getMap();
+
+            // Activityが初回で生成されたとき
+            if (savedInstanceState == null) {
+
+                // MapFragmentのオブジェクトをセット
+                mapFragment.setRetainInstance(true);
+
+                // 地図の初期設定を行うメソッドの呼び出し
+                mapInit();
+            }
+        }
+        // GoogleMapが使用不可のときのためにtry catchで囲っています。
+        catch (Exception e) {
+        }
     }
 
+    // 地図の初期設定メソッド
+    private void mapInit() {
+
+        // 地図タイプ設定
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        // 現在位置ボタンの表示を行なう
+        googleMap.setMyLocationEnabled(true);
+
+        // めがね会館の位置、ズーム設定
+        CameraPosition camerapos = new CameraPosition.Builder()
+                .target(new LatLng(35.942756,136.198842)).zoom(15.5f).build();
+
+        // 地図の中心の変更する
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(camerapos));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +85,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
